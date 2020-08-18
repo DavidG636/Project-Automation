24793c5824b1ead4f5a07e64a20ac7b39f7a1b3a
DavidG636 <32344996+DavidG636@users.noreply.github.com>
2020-08-18 14:01:54 -0400

    Add missing "<"

--- createProject.bat
+++ createProject.bat
@@ -5,7 +5,7 @@ type nul > index.html
 type nul > script.js
 type nul > styles.css
 type nul > README.md
-@echo "<!DOCTYPE html><html><head><meta charset="UTF-8"><title>%1</title>link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"><link rel="stylesheet" href="styles.css" type="text/css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script><script src='script.js' type="text/javascript"></script></head><body></body></html>" >> index.html
+@echo "<!DOCTYPE html><html><head><meta charset="UTF-8"><title>%1</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"><link rel="stylesheet" href="styles.css" type="text/css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script><script src='script.js' type="text/javascript"></script></head><body></body></html>" >> index.html
 git init
 git add --all
 git commit -m "Initial Commit"
